38ca97516b05a29ce32d0f4a8cf065488a8f2ed9
BuildTools <unconfigured@null.spigotmc.org>
2022-04-06 20:00:16 -0500

    Setup Recycler View

--- app/src/main/java/com/example/photojournal/NewPhotoFragment.java
+++ app/src/main/java/com/example/photojournal/NewPhotoFragment.java
@@ -10,26 +10,23 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NewEntryFragment#newInstance} factory method to
+ * Use the {@link NewPhotoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewEntryFragment extends Fragment {
+public class NewPhotoFragment extends Fragment {
 
-    public NewEntryFragment() {
-        super(R.layout.fragment_new_entry);
+    public NewPhotoFragment() {
+        super(R.layout.fragment_new_photo);
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment NewEntryFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static NewEntryFragment newInstance(String param1, String param2) {
-        NewEntryFragment fragment = new NewEntryFragment();
+    public static NewPhotoFragment newInstance() {
+        NewPhotoFragment fragment = new NewPhotoFragment();
 
         return fragment;
     }
@@ -44,6 +41,6 @@ public class NewEntryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_entry, container, false);
+        return inflater.inflate(R.layout.fragment_new_photo, container, false);
     }
 }
--- app/src/main/java/com/example/photojournal/PhotosListFragment.java
+++ app/src/main/java/com/example/photojournal/PhotosListFragment.java
@@ -11,14 +11,14 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EntriesListFragment#newInstance} factory method to
+ * Use the {@link PhotosListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EntriesListFragment extends Fragment {
+public class PhotosListFragment extends Fragment {
 
 
-    public EntriesListFragment() {
-        super(R.layout.fragment_entries_list);
+    public PhotosListFragment() {
+        super(R.layout.fragment_photos_list);
     }
 
     /**
@@ -27,11 +27,11 @@ public class EntriesListFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EntriesListFragment.
+     * @return A new instance of fragment PhotosListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EntriesListFragment newInstance(String param1, String param2) {
-        EntriesListFragment fragment = new EntriesListFragment();
+    public static PhotosListFragment newInstance(String param1, String param2) {
+        PhotosListFragment fragment = new PhotosListFragment();
 
         return fragment;
     }
@@ -46,7 +46,7 @@ public class EntriesListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_entries_list, container, false);
+        return inflater.inflate(R.layout.fragment_photos_list, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
